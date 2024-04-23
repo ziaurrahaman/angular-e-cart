6fcab47919680ffeeeaa4155ac8e9a564e96778c
Ziaur/ESD/Md. Ziaur Rahman (Email: mdziaur.rahman@reddotdigitalit.com) <MdZiaur.Rahman@reddotdigitalit.com>
2024-04-23 17:29:11 +0600

    custom directives added

--- src/app/container/product-detail/product-detail.component.ts
+++ src/app/container/product-detail/product-detail.component.ts
@@ -2,11 +2,13 @@ import { Component, Input } from '@angular/core';
 import { Product } from '../../Models/Product';
 import { ProductListComponent } from '../product-list/product-list.component';
 import { NgFor, NgIf } from '@angular/common';
+import { SetBackgroundDirective } from '../../CustomDirectives/set-background.directive';
+import { AppHoverDirective } from '../../CustomDirectives/app-hover.directive';
 
 @Component({
   selector: 'product-detail',
   standalone: true,
-  imports: [NgIf, NgFor],
+  imports: [NgIf, NgFor, SetBackgroundDirective, AppHoverDirective],
   templateUrl: './product-detail.component.html',
   styleUrl: './product-detail.component.css',
 })
--- src/app/container/product-list/product/product.component.ts
+++ src/app/container/product-list/product/product.component.ts
@@ -2,11 +2,12 @@ import { NgIf } from '@angular/common';
 import { NgStyle } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { Product } from '../../../Models/Product';
+import { HighlightsDirective } from '../../../CustomDirectives/highlights.directive';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [NgStyle, NgIf],
+  imports: [NgStyle, NgIf, HighlightsDirective],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css',
 })
